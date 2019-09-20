1e7d9ec0dafbd29338f0f62187749e3806e1b0bb
Zach Brown <zdbrwn@gmail.com>
2019-09-19 21:10:59 -0700

    changed response to always be 200

--- app/controllers/teachable_webhook_controller.rb
+++ app/controllers/teachable_webhook_controller.rb
@@ -9,15 +9,12 @@ class ::Teachable::TeachableWebhookController < ApplicationController
   COURSES = [302950]
 
   def index 
-    unless is_valid?
-      raise Discourse::InvalidAccess.new
+    if is_valid?
+      case event['type'].downcase
+        when 'enrollment.created'
+          Jobs.enqueue(:sync_teachable_users, json: event)
+      end
     end
-    
-    case event['type'].downcase
-      when 'enrollment.created'
-        Jobs.enqueue(:sync_teachable_users, json: event)
-    end
-
     
     render body: nil, status: 200
   end
@@ -29,10 +26,6 @@ class ::Teachable::TeachableWebhookController < ApplicationController
   end
 
   def is_valid?
-    begin
-      TRIGGERS.include?(event['type'].downcase) and COURSES.include?(event['object']['course_id'])
-    rescue
-      false
-    end
+    TRIGGERS.include?(event['type'].downcase) and COURSES.include?(event['object']['course_id'])
   end
 end
