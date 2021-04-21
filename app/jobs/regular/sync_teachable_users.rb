e5d8cafc1719f2c4a4053808b6e77b332ef2b151
Zach <zdbrwn@gmail>
2021-04-20 21:28:32 -0700

    fix?

--- app/jobs/regular/sync_teachable_users.rb
+++ app/jobs/regular/sync_teachable_users.rb
@@ -4,15 +4,13 @@ module ::Jobs
       json = args[:json]
       email = json['object']['user']['email']
       user_id = json['object']['user']['id']
-
-      # https://github.com/discourse/discourse/blob/master/app/models/invite.rb
-      invited_by = User.find_by_id(-1)
       invite = Invite.generate(invited_by, {
         email: email,
         skip_email: true
       })
 
-      TeachableInviteMailer.send_invite(invite)
+      message = TeachableInviteMailer.build_invite(invite)
+      Email::Sender.new(message, :invite).send
     end
   end
 end
--- app/mailers/teachable_invite_mailer.rb
+++ app/mailers/teachable_invite_mailer.rb
@@ -3,8 +3,8 @@ require_dependency 'email/message_builder'
 class TeachableInviteMailer < ActionMailer::Base
   include Email::BuildEmailHelper
 
-  def send_invite(invite)
-  	message = build_email(
+  def build_invite(invite)
+  	build_email(
   		invite.email,
 		template: 'teachable_invite_mailer',
 		site_domain_name: Discourse.current_hostname,
@@ -12,7 +12,5 @@ class TeachableInviteMailer < ActionMailer::Base
 		site_description: SiteSetting.site_description,
 	    invite_link: "#{Discourse.base_url}/invites/#{invite.invite_key}"
 	)
-	
-	Email::Sender.new(message, :invite).send	
   end
 end
